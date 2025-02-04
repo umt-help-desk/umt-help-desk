08fe91acea0793e35d83c6a9e0823c7819e2507d
Kaleemullah_Younas <125406268+Kaleemullah-Younas@users.noreply.github.com>
2025-02-05 02:31:25 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/umt-help-desk.github.io/' : '',
+  basePath: isProd ? '/umt-help-desk.github.io' : '',
+  trailingSlash: true,
+};
+
+
+/**const nextConfig = {
 }
 
-module.exports = nextConfig
+module.exports = nextConfig*/
